cd3a9a13b818b69b1d7cfa777ab8ab1e7a7090e9
jose <josetorres.asir@gmail.com>
2020-09-11 17:04:28 +0200

    Cambio en linea 6 de  src/pages/QuienSoy.js

--- src/pages/QuienSoy.js
+++ src/pages/QuienSoy.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function QuienSoy() {
   return (
     <div>
-      <h1>Quien Soy:</h1>
+      <h1>Quien Soy yo aquí:</h1>
       <h2>Jose Vicente Torres.</h2>
     </div>
   );
