c3a21f4805dd3354c9d2afcca34b9677169857d1
jose <josetorres.asir@gmail.com>
2020-09-11 17:12:59 +0200

    Modificamos linea 6 de src/pages/Contacto.js

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contacto() {
   return (
     <div>
-      <h1>Estamos en contacto</h1>
+      <h1>Estamos en contacto del proyecto.</h1>
     </div>
   );
 }
