56d85ae288135f8bb4276acd92b58e6293ef4446
jose <josetorres.asir@gmail.com>
2020-09-11 17:02:02 +0200

    cambios linea 7 de src/pages/QuienSoy.js

--- src/pages/QuienSoy.js
+++ src/pages/QuienSoy.js
@@ -4,7 +4,7 @@ export default function QuienSoy() {
   return (
     <div>
       <h1>Quien Soy:</h1>
-      <h2>Jose Vicente de la Cruz.</h2>
+      <h2>Jose Vicente Torres.</h2>
     </div>
   );
 }
